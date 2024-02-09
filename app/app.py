46870038d40d5e1f2e08cba3bebbab444451f900
medsriha <medsriha@gmail.com>
2024-02-08 21:30:13 -0600

    notebook

--- app/app.py
+++ app/app.py
@@ -1,25 +1,36 @@
 from flask import Flask, request, jsonify
 import requests
 import json
+from collections import deque
 from sentence_transformers import SentenceTransformer
 
 
 app = Flask(__name__)
-
 model = SentenceTransformer('thenlper/gte-base')
+queue = deque()
 
 def distance(v):
     return v[0] @ v[1]
 
-@app.route('/infr', methods=['POST'])
+def process_pairs(pairs, n_batch = 100):
+    prediction = []
+    for i in range(n_batch):
+        prediction.append(1)
+
+    return prediction
+
+@app.route('/infr', methods=['POST']ççç)
 def get():
     data = request.json
     id_ = data.get('id')
     pairs = data.get('pairs')
-    enc = model.encode(pairs)
-    # Distance between v1 and v2
-    mag = distance(enc)
+    
+    queue.extend(pairs)
 
-    return jsonify({'prediction': str(mag)})
+    if len(queue) > 100:
+        predictions = process_pairs(pairs)
+        queue.clear()
+        return jsonify({'prediction': predictions})
+    return jsonify({'prediction': ""})
 
       
